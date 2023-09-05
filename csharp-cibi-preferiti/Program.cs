e9cf4789d6a226fa8369496610fa2bcd8f356117
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-09-05 20:50:07 +0200

    fatto esercizio bonus

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.ComponentModel.Design;
 
 Console.WriteLine("Hello, World!");
-string[] cibiPreferiti = { "gelato", "ciccolato", "patatine", "biscotti", "pizza", "cotolette", "pollo fritto" };
+string[] cibiPreferiti = { "gelato", "ciccolato", "patatine", "biscotti", "pizza", "cotolette", "pollo fritto", "pasta", "o", "w", "a", "p"};
 
 Console.WriteLine($"La classifica è composta da: {cibiPreferiti.Length} cibi");
 
@@ -15,4 +16,16 @@ for(var i = 0; i < cibiPreferiti.Length; i++)
 Console.Write("Top cibo: ");
 Console.WriteLine(cibiPreferiti[0]);
 Console.Write("Cibo preferito ma non troppo: ");
-Console.WriteLine(cibiPreferiti[6]);
+Console.WriteLine(cibiPreferiti[cibiPreferiti.Length-1]);
+
+
+double ciboMediano = cibiPreferiti.Length / 2;
+
+if(cibiPreferiti.Length % 2 == 0)
+{
+    Console.Write($"Il cibo mediano è:  {cibiPreferiti[(int)ciboMediano-1]} e {cibiPreferiti[(int)ciboMediano]}");
+}else
+{
+    Console.Write($"Il cibo mediano è:  {cibiPreferiti[(int)ciboMediano]}");
+}
+
