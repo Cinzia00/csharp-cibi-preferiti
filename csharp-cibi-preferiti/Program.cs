2e611b83c5babae2a3ddb1b2612d5f28c0c4831d
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-09-05 17:57:49 +0200

    Aggiungere i file di progetto.

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+string[] cibiPreferiti = { "gelato", "ciccolato", "patatine", "biscotti", "pizza", "cotolette", "pollo fritto" };
+
+Console.WriteLine($"La classifica è composta da: {cibiPreferiti.Length} cibi");
+
+for(var i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.Write(cibiPreferiti[i]);
+    Console.WriteLine(" " + i);
+}
+
+Console.Write("Top cibo: ");
+Console.WriteLine(cibiPreferiti[0]);
+Console.Write("Cibo preferito ma non troppo: ");
+Console.WriteLine(cibiPreferiti[6]);
